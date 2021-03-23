4cb429ab4aa41c2267de4ea52a09bc0f323a8df6
Nikita <nekytnp@gmail.com>
2021-03-23 12:08:48 +0200

    Установка и настройка Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,9 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.action_mailer.default_url_options = { host: 'https://intense-cliffs-20209.herokuapp.com/', port: 3000 }
+
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pages#landing'
+  root 'pages#landing' # такая форма необходима для Devise
   get  'pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
